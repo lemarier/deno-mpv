c75acf7a491bd127ff759ca57ed56c8c14eeb34b
David L <david@lemarier.ca>
2020-06-01 13:45:35 -0400

    Fix lib command

--- build.rs
+++ build.rs
@@ -16,9 +16,11 @@ fn main() {
     let archive_dir = Path::new(&src_dir).join("archive.zip");
     let mut archive_file = fs::File::create(archive_dir).unwrap();
     let mut download_url = root_url + "/win32.zip";
+    let mut arch_type = "X86";
 
     if env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "64" {
         download_url = root_url + "/win32-x64.zip"
+        arch_type = "X64"
     }
 
     reqwest::get(download_url)
@@ -34,8 +36,13 @@ fn main() {
         .output()
         .expect("failed to unzip archive");
 
+    // should be run only from GH_ACTIONS
     if env::var("DENO_BUILD_MODE").unwrap() == "release" {
-        Command::new("lib /def:mpv.def /name:mpv-1.dll /out:mpv.lib /MACHINE:X64")
+        Command::new("lib")
+            .args("/def:mpv.def")
+            .args("/name:mpv-1.dll")
+            .args("/out:mpv.lib")
+            .args("/MACHINE:" + &arch_type)
             .current_dir(&out_dir)
             .output()
             .expect("failed to build mpv.lib");
